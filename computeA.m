70d099e3af78e1fced20c2b048f60a4b34c53c92
bielgf <bielgf30@gmail.com>
2026-05-08 09:05:18 +0200

    small bug fixed

--- computeA.m
+++ computeA.m
@@ -31,24 +31,22 @@ function [A11,A12,B1] = computeA(Ndiv, Tc1, Xc1, X1, l1, ca1, sa1, Qinf, X2, l2,
         end
 
         for jj = 1:Ndiv % Aij matrix
-            if jj ~= ii
-                Xc_ij_pan = (Xc1(ii,1) - X2(jj,1))*ca2(jj) - (Xc1(ii,2) - X2(jj,2))*sa2(jj);
-                Zc_ij_pan = (Xc1(ii,1) - X2(jj,1))*sa2(jj) + (Xc1(ii,2) - X2(jj,2))*ca2(jj);
+            Xc_ij_pan = (Xc1(ii,1) - X2(jj,1))*ca2(jj) - (Xc1(ii,2) - X2(jj,2))*sa2(jj);
+            Zc_ij_pan = (Xc1(ii,1) - X2(jj,1))*sa2(jj) + (Xc1(ii,2) - X2(jj,2))*ca2(jj);
 
-                r1 = sqrt(Xc_ij_pan^2 + Zc_ij_pan^2);
-                r2 = sqrt((Xc_ij_pan - l2(jj))^2 + Zc_ij_pan^2);
+            r1 = sqrt(Xc_ij_pan^2 + Zc_ij_pan^2);
+            r2 = sqrt((Xc_ij_pan - l2(jj))^2 + Zc_ij_pan^2);
 
-                theta1 = atan2(Zc_ij_pan,Xc_ij_pan);
-                theta2 = atan2(Zc_ij_pan,Xc_ij_pan - l2(jj));
+            theta1 = atan2(Zc_ij_pan,Xc_ij_pan);
+            theta2 = atan2(Zc_ij_pan,Xc_ij_pan - l2(jj));
 
-                u_ij_pan = (theta2 - theta1)/(2*pi);
-                w_ij_pan = (1/(4*pi))*log(r2^2/r1^2);
+            u_ij_pan = (theta2 - theta1)/(2*pi);
+            w_ij_pan = (1/(4*pi))*log(r2^2/r1^2);
 
-                u_ij = u_ij_pan*ca2(jj) + w_ij_pan*sa2(jj);
-                w_ij = -u_ij_pan*sa2(jj) + w_ij_pan*ca2(jj);
+            u_ij = u_ij_pan*ca2(jj) + w_ij_pan*sa2(jj);
+            w_ij = -u_ij_pan*sa2(jj) + w_ij_pan*ca2(jj);
 
-                A12(ii,jj) = dot([u_ij,w_ij],Tc1(ii,:));
-            end
+            A12(ii,jj) = dot([u_ij,w_ij],Tc1(ii,:));
         end
 
     end
